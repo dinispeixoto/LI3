7c9efc798723f643ba551f22a4817e8d9fe420f4
Ricardo Pereira <Ricardo@MacBook-Air-de-Ricardo.local>
2016-03-13 08:51:42 +0000

    Trabalho + recente

--- valida.c
+++ valida.c
@@ -1,32 +1,70 @@
 #include "valida.h"
 
-// Conta quantas linhas do ficheiro com os clientes são válidas, e aloca num array.
-int valClientsP(FILE *file, char** array){
-	
+// Conta quantas linhas dos ficheiros (Clientes e Produtos) são válidas, e aloca num array.
+// Vamos reservar um array em que as strings têm diferente tamanho:
+// Array de Clientes : tamanho = SIZE_CLIENTS
+// Array de Produtos : tamanho = SIZE_PRODUCTS
+int valCliProd(FILE *file, char** array,int size){
+
 	int validated=0; 
 	char buffer[SIZE_BUFFER],*line;
 
-
 	while(fgets(buffer,SIZE_BUFFER,file)!=NULL){
 
-	 	//strtoken 
-	 	line=strtok(buffer,"\r\n");
+		line=strtok(buffer,"\r\n");
 
-	 	//alocar espaço para a string e copia-la para o array(VER O SIZE)
-	 	array[validated] = (char*)malloc(SIZE_CLIENTS * sizeof(char));
-	 	strcpy(array[validated],line);
-	 	validated++;
-	 }
-	 	
+		//alocar espaço para a string e copia-la para o array.
+		array[validated] = (char*)malloc(size * sizeof(char));
+		strcpy(array[validated],line);
+		validated++;
+	}
+		
 	return validated;
 }
 
+// Conta quantas linhas do ficheiro com as vendas são válidas, e aloca num array.
+int valSales(FILE *file,char** clients,char** products,Vendas* sales){
+
+	char buffer[SIZE_BUF_SALES],*line;
+	int validated=0,r;
+    char *clie[SIZE_CLIENTS],*prod[SIZE_PRODUCTS];
+    int mes,filial,quant;
+    float preco;
+    char infoP;
+
+	while(fgets(buffer,SIZE_BUF_SALES,file)!=NULL){
+		
+		line = strtok(buffer,"\r\n");
+
+		// verificar, em caso positivo alocar espaço para a string e copia-la para o array.
+		r = partCheck(line,clients,products,clie,prod,&mes,&filial,&quant,&preco,&infoP);
+		if(r){
+			sales[validated] = malloc(sizeof(struct vendas));
+			strcpy(sales[validated]->client,*clie);
+			strcpy(sales[validated]->product,*prod);
+			sales[validated]->price=preco;
+			sales[validated]->quantity=quant;
+			sales[validated]->infoPromo=infoP;
+			sales[validated]->filial=filial;
+			sales[validated]->mes=mes;
+			validated+=r;
+		}
+	}
+	return validated;
+}
+
+// ############################################ FUNÇÕES AUXILIARES ################################################################################################################
+
+
 //Função auxiliar que verifica se um dado produto e cliente existem.
-int exist (char * prod, char * client, char** clients, char** products){
+int exist(char* prod, char* client, char** clients, char** products){
     int i,vc=0,vp=0;
 
-    for(i=0;i<171008; i++){
-         if(i<16384 && !vc){
+    int sizeClients = sizeArray(clients); // 16384
+    int sizeProducts = sizeArray(products); // 171008
+
+    for(i=0;i<sizeProducts; i++){
+         if(i<sizeClients && !vc){
          	if(!vc && strcmp(client,clients[i])==0) vc=1;
 
          }
@@ -41,54 +79,33 @@ int exist (char * prod, char * client, char** clients, char** products){
 
 //Função que reparte um linha de venda, e verifica se a linha é válida,ou seja, se o produto e cliente exitem, 
 //e se os outros parametros estao corretos. 
-int repartirVerificar (char* line, char** clients,char** products){
-    int r=0;
-    char *clie,*prod,*token;
-    int mes,filial,quant;
-    float preco;
-    char infoP;
+int partCheck(char* line, char** clients,char** products,char** clie,char** prod,int *month,int *filial,int *quant,float *price,char *infoP){
+	char *token;
+	int r=0, i;
+			
+	token = strtok(line, " ");
    
-    token = strtok(line, " ");
-   
-   for(int i=0;token != NULL;i++) 
-   {
-   	  switch(i){
-        case 0: prod=token;
-        case 1: preco=atof(token);
-        case 2: quant=atoi(token); 
-        case 3: infoP=token[0];
-        case 4: clie=token;
-        case 5: mes = atoi(token);
-        case 6: filial = atoi (token);
-   	  }
-      token = strtok(NULL, " ");
-   }
+	for(i=0;token != NULL;i++){
+		switch(i){
+			case 0: *prod = token; break;
+			case 1: *price = atof(token); break;
+			case 2: *quant = atoi(token); break;
+			case 3: *infoP = token[0]; break;
+			case 4: *clie = token; break;
+			case 5: *month = atoi(token); break;
+			case 6: *filial = atoi(token); break;
+		}
+			token = strtok(NULL, " ");
+	}
 
-    if(exist(prod,clie,clients,products) && testSales(preco, quant, infoP, mes, filial)) r=1;
-    return r;
+	if(exist(*prod,*clie,clients,products) && testSales(*price, *quant, *infoP, *month, *filial)) r=1;
+	return r;
 }
 
-// Conta quantas linhas do ficheiro com as vendas são válidas.
-int valSales(FILE *file,char** clients,char** products,char** sales){
+// Calcula o comprimentos dos Arrays de Clientes de Produtos.
+int sizeArray(char** array){
+	int size;
 
-	char buffer[SIZE_BUF_SALES],*line;
-	int validated,r;
-
-	 
-	while(fgets(buffer,SIZE_BUF_SALES,file)!=NULL){
-		
-		line=strtok(buffer,"\r\n");
-		r=repartirVerificar(line,clients,products);
-        if(r){
-            sales[validated] = (char*)malloc(SIZE_VENDAS * sizeof(char*));
-            strcpy(sales[validated],line);
-            }
-		validated+=r;
-    }
-
-	return validated;
+	for(size=0;array[size]!=NULL;size++);
+	return size;
 }
-
-// 1. APAGAR OS DEFINES QUE NÃO SÃO UTILIZADOS
-// 2. A valSales não está feita, faz um bocado da estrutura dela ou assim que eu depois faço o resto se quiseres.
-// 3. As funções valProducts e valClients são praticamente iguais, só muda o espaço que usamos para alocar strings, se quiseres junta numa só.
--- valida.h
+++ valida.h
@@ -1,37 +1,38 @@
 #ifndef __VALIDA_H__
 #define __VALIDA_H__
 
-#define LETRAS_C 1
-#define LETRAS_P 2
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "testes.h"
+
 #define SIZE_BUFFER 64
 #define SIZE_BUF_SALES 128
 
 #define SIZE_CLIENTS 6 //CONTAR COM O /0
 #define SIZE_PRODUCTS 7 // CONTAR COM O /0
-#define SIZE_VENDAS 29// 
+#define SIZE_SALES 29 // 
 
 #define MAX_CLIENTS 20000
 #define MAX_PRODUCTS 200000
 #define MAX_SALES 1000000
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-#include "testes.h"
 
 typedef struct vendas{
-   char* client,product;
+   char client[SIZE_CLIENTS];
+   char product[SIZE_PRODUCTS];
    float price;
    int quantity;
    char infoPromo;
    int filial;
-   struct vendas *esq,*dir;
-}Vendas[MAX_SALES];
+   int mes;
+}*Vendas;
 
-int valClientsP(FILE *,char**);
-int valSales(FILE *,char** ,char**,char** );
-
+int valCliProd(FILE *,char**,int);
+int valSales(FILE *,char**,char**,Vendas* );
+int exist(char*, char*, char**,char**);
+int partCheck(char*, char**,char**,char** ,char** ,int *,int *,int *,float *,char *);
+int sizeArray(char**);
 
 #endif
