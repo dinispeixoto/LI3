569dd943b81446ab588fa25080f25560eef8e9c6
Ricardo Pereira <ricardo.agp.914@gmail.com>
2016-04-19 11:38:36 +0100

    correcao de falhas de memoria

--- src/main.c
+++ src/main.c
@@ -23,24 +23,9 @@ int main(){
 	for(i=0;i<3;i++)
 		arrayFiliais[i] = initFilial();
 
-	/*interpretador(CatClients,CatProducts,arrayFiliais,fact);*/
-	getFile(CatClients,CatProducts,arrayFiliais,fact);
+	interpretador(CatClients,CatProducts,arrayFiliais,fact);
+	/*getFile(CatClients,CatProducts,arrayFiliais,fact);*/
 	
-	time_t begin_clients;
-	time_t end_clients;
-	double time_elapsed_clients = 0;
-	getchar();
-	begin_clients = clock();
-
-	LISTA_STRINGS ls;
-	ls=querie10(arrayFiliais[0],fact,5,1);
-
-	end_clients = clock();
-
-	time_elapsed_clients = (double) (end_clients - begin_clients) / CLOCKS_PER_SEC;
-	printf("	Tempo : %f s.\n",time_elapsed_clients);
-
-	printLS(ls);
 
 	return 0;
 }
